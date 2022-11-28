87ff5a6eea5d1b46f0db2e5ea9086a6ae4177356
insian <patrasoham1512@gmail.com>
2022-11-29 01:54:48 +0530

    maybe final

--- src/pages/database.js
+++ src/pages/database.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,8 +19,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const storage = getStorage(app);
-export { database, storage };
+export { database};
 
 const admindb = "AdminLogin/";
 const lbdb = "LbLogin/";
